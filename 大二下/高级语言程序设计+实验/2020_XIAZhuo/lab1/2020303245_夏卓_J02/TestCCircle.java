2d3de5bc7311b58e4a87e9384600d067c90bc15b
xiazhuo <79448622+xiazhuo@users.noreply.github.com>
2023-07-30 21:53:27 +0800

    Add files via upload

---
+++ 大二下/高级语言程序设计+实验/2020_XIAZhuo/lab1/2020303245_夏卓_J02/TestCCircle.java
@@ -0,0 +1,7 @@
+public class TestCCircle {
+	public static void main(String args[]) {
+		CCircle cir1 = new CCircle(2.0);
+		System.out.println("radius=" + cir1.getRadius());
+		System.out.println("Area=" + cir1.getArea());
+	}
+}
---
+++ 大二下/高级语言程序设计+实验/2020_XIAZhuo/lab2/2020303245_夏卓_J05/DemoPackage.java
@@ -0,0 +1,7 @@
+import MainPackage.SubClass;
+
+public class DemoPackage {
+    public static void main(String[] args) {
+        SubClass s = new SubClass(3);
+    }
+}
